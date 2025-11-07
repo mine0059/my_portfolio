d648f046b6ed6916e180f93d8923980daa21162c
CoderBlack25 <Koderblac@gmail.com>
2025-11-07 23:47:42 +0100

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -140,8 +140,10 @@ const Navbar = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.4 }}
         >
-          <p>A Nigeria-based front-end developer passionate about</p>
-          <p>building accessible and user friendly web products.</p>
+          <p>
+            A Nigeria-based front-end developer passionate about <br />
+            building accessible and user friendly web products.
+          </p>
         </motion.div>
 
         <motion.div
